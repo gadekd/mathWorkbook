c28d907771497058f7e332e4aeaee635565792bc
gadekd <danielwgadek@gmail.com>
2024-07-30 14:41:57 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mathWorkbook',
+  webDir: 'dist'
+};
+
+export default config;
